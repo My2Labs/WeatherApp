c6e3b5e9f78ab942b0c5e40a6b7d31ac7c08e757
Sharron H <sharronhemenway@outlook.com>
2023-08-25 11:09:29 -0500

    edit views.py

--- lookup/views.py
+++ lookup/views.py
@@ -86,39 +86,95 @@ def enter(request):
 def airviews(request):
     import json
     import requests
-    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=37201&distance=25&API_KEY=60CCA6E7-534C-4471-A04F-0DAA8BADD8A7")
+
+    if request.method == "POST":
+        zipcode = request.POST['zipcode']
+        api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zipcode + "&distance=25&API_KEY=60CCA6E7-534C-4471-A04F-0DAA8BADD8A7")
+
+        try: 
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error.."
         
-    try: 
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error.."
-    
-
-    if api[1]['Category']['Name'] == "Good":
-        category_description = "Air quality is considered satisfactory, and air pollution poses little or no risk."  
-        category_color = "good" 
-    elif api[1]['Category']['Name'] == "Moderate":
-        category_description = "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."  
-        category_color = "moderate"     
-    elif api[1]['Category']['Name'] == "Unhealthy for Sensitive Groups":
-        category_description = "Although general public is not likely to be affected at this air quality range, people with lung disease, older adults and children are at a greater risk from exposure to ozone, whereas persons with heart and lung disease, older adults and children are at greater risk from the presence of particles in the air."  
-        category_color = "usg"     
-    elif api[1]['Category']['Name'] == "Unhealthy":
-        category_description = "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."  
-        category_color = "unhealthy"     
-    elif api[1]['Category']['Name'] == "Very Unhealthy":
-        category_description = "Health alert: Everyone may experience more serious health effects."  
-        category_color = "veryunhealthy"     
-    elif api[1]['Category']['Name'] == "Hazardous":
-        category_description = "Health warnings of emergency conditions.  The entire population is more likely to be affected."  
-        category_color = "hazardous"  
 
 
-    return render(request, 'airviews.html', {
-        'api': api, 
-        'category_description': category_description, 
-        'category_color': category_color,
-        })
+        if api[1]['Category']['Name'] == "Good":
+            category_description = "Air quality is considered satisfactory, and air pollution poses little or no risk."  
+            category_color = "good" 
+        elif api[1]['Category']['Name'] == "Moderate":
+            category_description = "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."  
+            category_color = "moderate"     
+        elif api[1]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+            category_description = "Although general public is not likely to be affected at this air quality range, people with lung disease, older adults and children are at a greater risk from exposure to ozone, whereas persons with heart and lung disease, older adults and children are at greater risk from the presence of particles in the air."  
+            category_color = "usg"     
+        elif api[1]['Category']['Name'] == "Unhealthy":
+            category_description = "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."  
+            category_color = "unhealthy"     
+        elif api[1]['Category']['Name'] == "Very Unhealthy":
+            category_description = "Health alert: Everyone may experience more serious health effects."  
+            category_color = "veryunhealthy"     
+        elif api[1]['Category']['Name'] == "Hazardous":
+            category_description = "Health warnings of emergency conditions.  The entire population is more likely to be affected."  
+            category_color = "hazardous"  
+
+
+
+        return render(request, 'airviews.html', {
+            'api': api, 
+            'category_description': category_description, 
+            'category_color': category_color,
+            'zipcode': zipcode
+            })
+
+    else:
+        api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=37201&distance=25&API_KEY=60CCA6E7-534C-4471-A04F-0DAA8BADD8A7")
+            
+        try: 
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error.."
+        
+
+        if api[1]['Category']['Name'] == "Good":
+            category_description = "Air quality is considered satisfactory, and air pollution poses little or no risk."  
+            category_color = "good" 
+        elif api[1]['Category']['Name'] == "Moderate":
+            category_description = "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution."  
+            category_color = "moderate"     
+        elif api[1]['Category']['Name'] == "Unhealthy for Sensitive Groups":
+            category_description = "Although general public is not likely to be affected at this air quality range, people with lung disease, older adults and children are at a greater risk from exposure to ozone, whereas persons with heart and lung disease, older adults and children are at greater risk from the presence of particles in the air."  
+            category_color = "usg"     
+        elif api[1]['Category']['Name'] == "Unhealthy":
+            category_description = "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects."  
+            category_color = "unhealthy"     
+        elif api[1]['Category']['Name'] == "Very Unhealthy":
+            category_description = "Health alert: Everyone may experience more serious health effects."  
+            category_color = "veryunhealthy"     
+        elif api[1]['Category']['Name'] == "Hazardous":
+            category_description = "Health warnings of emergency conditions.  The entire population is more likely to be affected."  
+            category_color = "hazardous"  
+
+
+        return render(request, 'airviews.html', {
+            'api': api, 
+            'category_description': category_description, 
+            'category_color': category_color,
+            })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -130,8 +186,6 @@ def airhtml(request):
 
     if request.method == "POST":
         zipcode = request.POST['zipcode']
-        output = calculate_result()
-        result = bubble_sort()
         api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=" + zipcode + "&distance=25&API_KEY=60CCA6E7-534C-4471-A04F-0DAA8BADD8A7")
     
         try: 
@@ -140,12 +194,10 @@ def airhtml(request):
             api = "Error.."
         
 
-        return render(request, 'airhtml.html', {'api': api, 'zipcode': zipcode, 'bubble_sort': bubble_sort, 'output': output, 'result': result})
+        return render(request, 'airhtml.html', {'api': api, 'zipcode': zipcode})
     
     else:  
         api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=37201&distance=25&API_KEY=60CCA6E7-534C-4471-A04F-0DAA8BADD8A7")
-        output = calculate_result()
-        result = bubble_sort()
 
         try: 
             api = json.loads(api_request.content)
@@ -153,4 +205,4 @@ def airhtml(request):
             api = "Error.."
 
 
-        return render(request, 'airhtml.html', {'api': api, 'result': result, 'output': output})
+        return render(request, 'airhtml.html', {'api': api})
